488e0b2e54eeb61334bc5a2e912ae32d085aae69
rk918jp <113189714+rk918jp@users.noreply.github.com>
2023-10-03 20:36:34 +0900

    通知のテスト

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from "react";
+
+
+const askPermission = () => {
+  Notification.requestPermission(function(result) {
+    if (result !== "granted") {
+      console.log("No notification permission granted!");
+    }
+  });
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    askPermission();
+  }, []);
   return <Component {...pageProps} />
 }
